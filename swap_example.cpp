bc3e48967b0ad6d97f7806fa5245707c86c1e9af
ggucci <74988971+ggucci@users.noreply.github.com>
2020-11-24 20:57:45 -0500

    Add files via upload

---
+++ swap_example.cpp
@@ -0,0 +1,20 @@
+
+using namspace std;
+#include <iostream>
+
+int main(){
+    // sum numbers 0 to 30 that arent dvisible by 3 or 7
+
+    const int MAX_INDEX = 30;
+    int sum - 0;
+    for(int i = 0; i <=MAX_INDEX; ++i){
+        if(i % 3 != 0 || i % 7 ! = 0){
+            
+        }
+    }
+
+
+
+
+
+}
